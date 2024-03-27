dafad466b654bbc187c0a823504d2d45c83661e3
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-03-27 19:45:12 +0600

    Добавлено открытие окна при отсутствии заданного файла

--- src/ru/danilakondr/md2writer/Application.java
+++ src/ru/danilakondr/md2writer/Application.java
@@ -3,6 +3,7 @@ package ru.danilakondr.md2writer;
 import com.sun.star.beans.PropertyValue;
 import com.sun.star.text.*;
 import com.sun.star.frame.*;
+import com.sun.star.ui.dialogs.*;
 import com.sun.star.uno.*;
 import com.sun.star.lang.*;
 import com.sun.star.comp.helper.Bootstrap;
@@ -13,18 +14,19 @@ import java.lang.RuntimeException;
 import java.nio.file.Path;
 
 public class Application {
-	private final String docPath;
+	private String docPath;
 	private String docURL;
     private XDesktop xDesktop;
 	private XTextDocument xDoc;
+	private XComponentContext xContext;
+	private XMultiComponentFactory xMCF;
 	private boolean success = false;
 	
-	public Application(String docPath) {
-		this.docPath = docPath;
-	}
+	public Application() {};
 
 	public static void main(String[] args) {
-		Application app = new Application(args[0]);
+		Application app = new Application();
+		app.parseCommandLine(args);
 
 		try {
 			app.run();
@@ -34,6 +36,15 @@ public class Application {
 		}
 		finally {
 			app.terminate();
+		}
+	}
+
+	public void parseCommandLine(String[] args) {
+		if (args.length < 1) {
+			docPath = null;
+		}
+		else {
+			docPath = args[0];
 		}
 	}
 
@@ -48,19 +59,76 @@ public class Application {
 		this.closeDocument();
 	}
 
-	private void tryToOpen() throws RuntimeException {
-		File f = new File(docPath);
-		if (!f.exists()) {
-			throw new RuntimeException("Cannot open file " + f);
+	private void tryToOpen() throws Exception {
+		if (docPath != null) {
+			File f = new File(docPath);
+			if (!f.exists()) {
+				throw new RuntimeException("Cannot open file " + f);
+			}
+
+			Path p = f.toPath();
+			this.docURL = p.toUri().toString();
+		}
+		else {
+			System.err.println("Trying to select a file");
+			pickFile();
 		}
 
-		Path p = f.toPath();
-		this.docURL = p.toUri().toString();
+		if (docURL == null)
+			throw new RuntimeException("File has not been specified");
 	}
-	
+
+	private void pickFile() throws Exception {
+		XComponent xComponent = null;
+
+		try {
+			Object oFilePicker = xMCF
+					.createInstanceWithContext(
+							"com.sun.star.ui.dialogs.FilePicker",
+							xContext
+					);
+			XFilePicker xFilePicker = UnoRuntime.queryInterface(
+					XFilePicker.class,
+					oFilePicker
+			);
+
+			XFilterManager xFilterManager = UnoRuntime.queryInterface(XFilterManager.class, xFilePicker);
+			xFilterManager.appendFilter("OpenDocument Text", "*.odt");
+			xFilterManager.appendFilter("Microsoft Word 2007", "*.docx");
+			xFilterManager.appendFilter("Microsoft Word 2003", "*.doc");
+			xFilterManager.appendFilter("Rich Text Format", "*.rtf");
+			xFilterManager.appendFilter("Any files", "*.*");
+
+			XInitialization xInit = UnoRuntime
+					.queryInterface(XInitialization.class, xFilePicker);
+			Short[] listAny = new Short[]{TemplateDescription.FILEOPEN_SIMPLE};
+			xInit.initialize(listAny);
+
+			xComponent = UnoRuntime.queryInterface(XComponent.class, xFilePicker);
+
+			XExecutableDialog xDialog = UnoRuntime
+					.queryInterface(XExecutableDialog.class, xFilePicker);
+			short nResult = xDialog.execute();
+
+			if (nResult == ExecutableDialogResults.OK) {
+				String[] sPathList = xFilePicker.getFiles();
+				if (sPathList.length > 0) {
+					docURL = sPathList[0];
+				}
+			}
+		}
+		catch (Exception e) {
+			System.err.println(e.getMessage());
+		}
+		finally {
+			if (xComponent != null)
+				xComponent.dispose();
+		}
+	}
+
 	private void bootstrap() throws java.lang.Exception {
-        XComponentContext xContext = Bootstrap.bootstrap();
-        XMultiComponentFactory xMCF = xContext.getServiceManager();
+        xContext = Bootstrap.bootstrap();
+		xMCF = xContext.getServiceManager();
 		Object oDesktop = xMCF.createInstanceWithContext(
                 "com.sun.star.frame.Desktop", xContext);
 		xDesktop = UnoRuntime.queryInterface(XDesktop.class, oDesktop);
