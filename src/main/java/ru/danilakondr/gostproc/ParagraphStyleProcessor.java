b4116dede33a7177b6f08f16837c4a3c5e7827b8
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-03-30 16:08:30 +0600

    v0.1.2: добавление установки стилей заголовков

--- src/main/java/ru/danilakondr/gostproc/ParagraphStyleProcessor.java
+++ src/main/java/ru/danilakondr/gostproc/ParagraphStyleProcessor.java
@@ -1,8 +1,8 @@
 package ru.danilakondr.gostproc;
 
-import com.sun.star.container.NoSuchElementException;
+import com.sun.star.awt.FontSlant;
+import com.sun.star.awt.FontWeight;
 import com.sun.star.container.XNameAccess;
-import com.sun.star.lang.WrappedTargetException;
 import com.sun.star.style.LineSpacing;
 import com.sun.star.style.LineSpacingMode;
 import com.sun.star.style.ParagraphAdjust;
@@ -15,14 +15,16 @@ import com.sun.star.beans.XPropertySet;
 /**
  * Установщик стилей абзацев. Оформляет стили абзацев в соответствии с
  * ГОСТом.
- * <p></p>
- * TODO: добавить оформление стилей заголовков
  *
  * @author Данила А. Кондратенко
  * @since 0.1.0
  */
 public class ParagraphStyleProcessor extends Processor {
     private final XNameAccess xParagraphStyles;
+
+    public enum Indent {
+        LEFT, PARAGRAPH, HEADING, CENTER, RIGHT
+    };
 
     public ParagraphStyleProcessor(XTextDocument xDoc) {
         super(xDoc);
@@ -53,28 +55,71 @@ public class ParagraphStyleProcessor extends Processor {
         alignLeft("Contents 1");
         alignLeft("Contents 2");
         alignLeft("Contents 3");
+        heading("Heading", Indent.HEADING, false);
+        heading("Heading 1", Indent.HEADING, true);
+        heading("Heading 2", Indent.HEADING, true);
+        heading("Heading 3", Indent.HEADING, true);
+        heading("Heading 4", Indent.HEADING, true);
+        heading("Contents Heading", Indent.CENTER, false);
     }
 
-    private void defaultParameters(XPropertySet xStyleProp) throws Exception {
+    private XPropertySet getStyleProperties(String styleName) throws Exception {
+        return UnoRuntime.queryInterface(
+                XPropertySet.class,
+                xParagraphStyles.getByName(styleName)
+        );
+    }
+
+    // TODO реализовать нумерацию
+    private void heading(String styleName, Indent indent, boolean numbered) throws Exception {
+        XPropertySet xStyleProp = getStyleProperties(styleName);
+
+        setParagraphStyleParameters(xStyleProp, indent, true);
+    }
+
+    private void setParagraphStyleParameters(XPropertySet xStyleProp,
+                                             Indent indent,
+                                             boolean bold)
+            throws Exception
+    {
         xStyleProp.setPropertyValue("ParaLineSpacing", new LineSpacing(LineSpacingMode.PROP, (short) 150));
-        xStyleProp.setPropertyValue("CharHeight", 14.0f);
         xStyleProp.setPropertyValue("ParaLeftMargin", 0);
         xStyleProp.setPropertyValue("ParaRightMargin", 0);
         xStyleProp.setPropertyValue("ParaTopMargin", 0);
         xStyleProp.setPropertyValue("ParaBottomMargin", 0);
         xStyleProp.setPropertyValue("ParaOrphans", (byte)0);
         xStyleProp.setPropertyValue("ParaWidows", (byte)0);
+
+        xStyleProp.setPropertyValue("CharHeight", 14.0f);
+        xStyleProp.setPropertyValue("CharFontName", "Liberation Serif");
+
+        xStyleProp.setPropertyValue("CharWeight", bold ? FontWeight.BOLD : FontWeight.NORMAL);
+        xStyleProp.setPropertyValue("CharPosture", FontSlant.NONE);
+
+        xStyleProp.setPropertyValue("ParaFirstLineIndent", 0);
+        switch (indent) {
+            case HEADING:
+                xStyleProp.setPropertyValue("ParaFirstLineIndent", 1250);
+            case LEFT:
+                xStyleProp.setPropertyValue("ParaAdjust", ParagraphAdjust.LEFT);
+                break;
+            case PARAGRAPH:
+                xStyleProp.setPropertyValue("ParaFirstLineIndent", 1250);
+                xStyleProp.setPropertyValue("ParaAdjust", ParagraphAdjust.STRETCH);
+                break;
+            case RIGHT:
+                xStyleProp.setPropertyValue("ParaAdjust", ParagraphAdjust.RIGHT);
+                break;
+            case CENTER:
+                xStyleProp.setPropertyValue("ParaAdjust", ParagraphAdjust.CENTER);
+                break;
+        }
     }
 
     private void firstIndent(String styleName) throws Exception {
-        XPropertySet xStyleProp = UnoRuntime.queryInterface(
-                XPropertySet.class,
-                xParagraphStyles.getByName(styleName)
-        );
+        XPropertySet xStyleProp = getStyleProperties(styleName);
 
-        defaultParameters(xStyleProp);
-        xStyleProp.setPropertyValue("ParaFirstLineIndent", 1250);
-        xStyleProp.setPropertyValue("ParaAdjust", ParagraphAdjust.STRETCH);
+        setParagraphStyleParameters(xStyleProp, Indent.PARAGRAPH, false);
     }
 
     private void alignLeft(String styleName) throws Exception {
@@ -83,8 +128,7 @@ public class ParagraphStyleProcessor extends Processor {
                 xParagraphStyles.getByName(styleName)
         );
 
-        defaultParameters(xStyleProp);
-        xStyleProp.setPropertyValue("ParaAdjust", ParagraphAdjust.LEFT);
+        setParagraphStyleParameters(xStyleProp, Indent.LEFT, false);
     }
 
     private void alignCenter(String styleName) throws Exception {
@@ -93,7 +137,6 @@ public class ParagraphStyleProcessor extends Processor {
                 xParagraphStyles.getByName(styleName)
         );
 
-        defaultParameters(xStyleProp);
-        xStyleProp.setPropertyValue("ParaAdjust", ParagraphAdjust.CENTER);
+        setParagraphStyleParameters(xStyleProp, Indent.CENTER, false);
     }
 }
