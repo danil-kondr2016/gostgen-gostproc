e7cd6f5cce2ae46df39d2d06b388cd406e44a63d
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-01 19:35:42 +0600

    StarMathFixer: добавление обработки вариантов символов латинского алфавита
    Преобразователь LaTeX -> MathML преобразует символы по типу \mathbb в символы Юникода.
    Задача - по мере сил преобразовать их в инструкции StarMath.

--- src/main/java/ru/danilakondr/templater/processing/StarMathFixer.java
+++ src/main/java/ru/danilakondr/templater/processing/StarMathFixer.java
@@ -26,6 +26,7 @@ public class StarMathFixer {
      */
     private static final Pattern right = Pattern.compile("([\\\\+\\-/&|=<>]|cdot|times|div|plusminus|minusplus)\\s*#");
 
+    private static final int LATIN_ALPHABET_SIZE = 26;
     /**
      * Обрабочик одной формулы на языке StarMath.
      *
@@ -38,6 +39,35 @@ public class StarMathFixer {
         Matcher rMatch = right.matcher(sFormula1);
         String sFormula2 = rMatch.replaceAll("$1 {} #");
 
-        return sFormula2;
+        return fixCharacters(sFormula2);
+    }
+
+    public static String fixCharacters(String formula) {
+        final int CODEPOINT_BOLD_CAPITAL_A = 119808;
+        final int CODEPOINT_BOLD_SMALL_A = 119834;
+        final int CODEPOINT_ITALIC_CAPITAL_A = 119860;
+        final int CODEPOINT_ITALIC_SMALL_A = 119886;
+        final int CODEPOINT_BOLD_ITALIC_CAPITAL_A = 119912;
+        final int CODEPOINT_BOLD_ITALIC_SMALL_A = 119938;
+
+        String f = formula;
+        f = fixLatinAlphabet(f, CODEPOINT_BOLD_CAPITAL_A, CODEPOINT_BOLD_SMALL_A, "{bold nitalic %c}");
+        f = fixLatinAlphabet(f, CODEPOINT_ITALIC_CAPITAL_A, CODEPOINT_ITALIC_SMALL_A, "{italic %c}");
+        f = fixLatinAlphabet(f, CODEPOINT_BOLD_ITALIC_CAPITAL_A, CODEPOINT_BOLD_ITALIC_SMALL_A, "{bold italic %c}");
+        return f;
+    }
+
+    private static String fixLatinAlphabet(String f, int firstCapital, int firstSmall, String replace) {
+        String x = f;
+        for (int i = 0; i < LATIN_ALPHABET_SIZE; i++) {
+            String capital = Character.toString(firstCapital + i);
+            String small = Character.toString(firstSmall + i);
+
+            String capReplacement = String.format(replace, (char) ('A' + i));
+            String smallReplacement = String.format(replace, (char) ('a' + i));
+
+            x = x.replace(capital, capReplacement).replace(small, smallReplacement);
+        }
+        return x;
     }
 }
