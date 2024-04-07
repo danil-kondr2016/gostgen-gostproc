29ffa90c7413f9c6427b5efaa45f85ff186cda9c
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-07 16:32:55 +0600

    LibreOffice: добавление чтения libreoffice.properties

--- src/main/java/ru/danilakondr/templater/LibreOffice.java
+++ src/main/java/ru/danilakondr/templater/LibreOffice.java
@@ -3,12 +3,12 @@ package ru.danilakondr.templater;
 import com.sun.star.uno.XComponentContext;
 import ooo.connector.BootstrapSocketConnector;
 
-import java.io.File;
+import java.io.*;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.io.IOException;
+import java.util.Properties;
 
 import org.apache.commons.lang3.SystemUtils;
 
@@ -23,6 +23,9 @@ import org.apache.commons.lang3.SystemUtils;
  * @since 0.1.0
  */
 public class LibreOffice {
+    private static final String LIBREOFFICE_HOME_PROPERTY = "libreoffice.home";
+
+    private static final String LIBREOFFICE_PROPERTIES_FILE = "libreoffice.properties";
     /**
      * Ищет LibreOffice.
      *
@@ -33,17 +36,12 @@ public class LibreOffice {
      *                     к директории
      */
     private static String find() throws LibreOfficeException, IOException {
-        // LIBREOFFICE_HOME - папка, где находятся исполняемые файлы
-        // LibreOffice.
-        // На Windows это обычно C:\Program Files\LibreOffice\program.
-        // На Linux это обычно /usr/lib/libreoffice/program.
-
-        String lohome = System.getenv("LIBREOFFICE_HOME");
-        if (lohome != null) {
-            File fLOHome = new File(lohome);
-            if (!fLOHome.exists())
+        String loHomePath = getLibreOfficeHomeSetting();
+        if (loHomePath != null) {
+            File loHomeDir = new File(loHomePath);
+            if (!loHomeDir.exists())
                 throw new LibreOfficeException("LibreOffice not found");
-            return lohome;
+            return loHomePath;
         }
 
         if (SystemUtils.IS_OS_WINDOWS)
@@ -54,6 +52,35 @@ public class LibreOffice {
             return findLibreOfficeInMacOS();
 
         throw new LibreOfficeException("Unrecognized platform");
+    }
+
+    private static String getLibreofficeHomeProperty(Path p) throws IOException {
+        String loHomePath = null;
+        Properties loProps = new Properties();
+
+        File propsFile = p.toFile();
+        if (propsFile.exists()) {
+            loProps.load(new InputStreamReader(new FileInputStream(propsFile)));
+        }
+
+        loHomePath = loProps.getProperty(LIBREOFFICE_HOME_PROPERTY);
+
+        return loHomePath;
+    }
+
+    private static String getLibreOfficeHomeSetting() throws IOException {
+        String loHomePath = System.getenv("LIBREOFFICE_HOME");
+        if (loHomePath == null) {
+            loHomePath = System.getProperty(LIBREOFFICE_HOME_PROPERTY);
+        }
+        if (loHomePath == null) {
+            loHomePath = getLibreofficeHomeProperty(Path.of(System.getProperty("user.home"), LIBREOFFICE_PROPERTIES_FILE));
+        }
+        if (loHomePath == null) {
+            loHomePath = getLibreofficeHomeProperty(Path.of(".", LIBREOFFICE_PROPERTIES_FILE));
+        }
+
+        return loHomePath;
     }
 
     private static String findLibreOfficeInWindows() throws LibreOfficeException, IOException {
