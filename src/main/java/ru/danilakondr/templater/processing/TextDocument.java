ba2526822f3dedb4225d68c73918509b433e7b88
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-07 14:04:02 +0600

    TextDocument: javadoc

--- src/main/java/ru/danilakondr/templater/processing/TextDocument.java
+++ src/main/java/ru/danilakondr/templater/processing/TextDocument.java
@@ -16,15 +16,41 @@ import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+/**
+ * Класс-обёртка над XTextDocument. Содержит методы для работы с объектами,
+ * которые принимают на вход обработчик и счётчик процесса.
+ *
+ * @author Данила А. Кондратенко
+ * @since 0.3.0
+ */
 public class TextDocument {
+    /**
+     * Документ.
+     */
     private final XTextDocument xDoc;
+    /**
+     * Сравнитель текстовых отрезков.
+     */
     private final XTextRangeCompare xCmp;
+    /**
+     * GUID типа объекта формул.
+     */
     private static final String MATH_FORMULA_GUID = "078B7ABA-54FC-457F-8551-6147e776a997";
-
+    /**
+     * Список всех формул в документе. Должен инициализироваться при первом
+     * вызове метода processFormulas().
+     */
     private final HashMap<String, Object> formulas;
-
+    /**
+     * Список всех секций в документе. Должен инициализирваться при первом
+     * вызове метода streamSections().
+     */
     private final HashMap<String, XTextSection> sections;
 
+    /**
+     * Интерфейс-обработчик объектов.
+     * @param <T> тип объекта
+     */
     @FunctionalInterface
     public interface ObjectProcessor<T> {
         void process(T object, XTextDocument xDoc);
@@ -38,11 +64,29 @@ public class TextDocument {
         this.sections = new HashMap<>();
     }
 
-    private boolean isRangeInside(XTextRange b, XTextRange a)
+    /**
+     * Проверяет, находится ли inner внутри outer.
+     *
+     * @param inner предполагаемый внутренний отрезок
+     * @param outer предполагаемый внешний отрезок
+     * @return true, если inner внутри outer; false в противном случае
+     */
+    private boolean isRangeInside(XTextRange inner, XTextRange outer)
     {
-        return xCmp.compareRegionStarts(a, b) >= 0 && xCmp.compareRegionEnds(a, b) <= 0;
+        return xCmp.compareRegionStarts(outer, inner) >= 0
+                && xCmp.compareRegionEnds(outer, inner) <= 0;
     }
 
+    /**
+     * Сканирует все формулы в документе. Метод вызывается один раз.
+     * Это необходимо для того, чтобы LibreOffice не входил в глубокую
+     * медитацию при обработке формул (видимо, из-за частых запросов к
+     * объектной структуре напрямую).
+     * <p>
+     * Сохраняет все формулы в <code>formulas</code>.
+     *
+     * @see TextDocument#formulas
+     */
     private void scanAllFormulas() throws Exception
     {
         if (!formulas.isEmpty())
@@ -73,7 +117,20 @@ public class TextDocument {
         }
     }
 
+    /**
+     * Сканирует все секции в документе. Метод вызывается один раз.
+     * Сканирование делается от греха подальше, чтобы не произошло глубокой
+     * медитации, как в случае с формулами.
+     * <p>
+     * Сохраняет все секции в <code>sections</code>.
+     *
+     * @see TextDocument#sections
+     * @see TextDocument#scanAllFormulas
+     */
     private void scanAllSections() throws Exception {
+        if (!sections.isEmpty())
+            return;
+
         XTextSectionsSupplier xSup = UnoRuntime
                 .queryInterface(XTextSectionsSupplier.class, xDoc);
         XNameAccess xSections = xSup.getTextSections();
@@ -85,7 +142,13 @@ public class TextDocument {
         }
     }
 
-    public TextDocument processParagraphs(ObjectProcessor<XTextContent> proc, ProgressCounter progress) throws Exception {
+    /**
+     * Обрабатывает все абзацы по порядку.
+     *
+     * @param processor обработчик абзаца
+     * @param progress счётчик прогресса
+     */
+    public TextDocument processParagraphs(ObjectProcessor<XTextContent> processor, ProgressCounter progress) throws Exception {
         XEnumerationAccess xEnumAccess = UnoRuntime
                 .queryInterface(XEnumerationAccess.class, xDoc.getText());
         XEnumeration xEnum = xEnumAccess.createEnumeration();
@@ -96,13 +159,19 @@ public class TextDocument {
                     .queryInterface(XTextContent.class, xEnum.nextElement());
 
             progress.next();
-            proc.process(xParagraph, xDoc);
+            processor.process(xParagraph, xDoc);
         }
 
         return this;
     }
 
-    public TextDocument processImages(ObjectProcessor<Object> proc, ProgressCounter progress) throws Exception {
+    /**
+     * Обрабатывает все изображения по порядку.
+     *
+     * @param processor обработчик изображения
+     * @param progress счётчик прогресса
+     */
+    public TextDocument processImages(ObjectProcessor<Object> processor, ProgressCounter progress) throws Exception {
         XNameAccess graphicObjects = UnoRuntime
                 .queryInterface(XTextGraphicObjectsSupplier.class, xDoc)
                 .getGraphicObjects();
@@ -111,26 +180,39 @@ public class TextDocument {
         progress.setTotal(names.length);
         for (String objId : names) {
             progress.next();
-            proc.process(graphicObjects.getByName(objId), xDoc);
+            processor.process(graphicObjects.getByName(objId), xDoc);
         }
 
         return this;
     }
 
-    public TextDocument processFormulas(ObjectProcessor<Object> proc, ProgressCounter progress) throws Exception {
+    /**
+     * Обрабатывает все формулы по порядку.
+     *
+     * @param processor обработчик формулы
+     * @param progress счётчик прогресса
+     */
+    public TextDocument processFormulas(ObjectProcessor<Object> processor, ProgressCounter progress) throws Exception {
         if (formulas.isEmpty())
             scanAllFormulas();
 
         progress.setTotal(formulas.size());
         formulas.forEach((k, v) -> {
             progress.next();
-            proc.process(v, xDoc);
+            processor.process(v, xDoc);
         });
 
         return this;
     }
 
-    public TextDocument processTablesInsideRange(XTextRange range, ObjectProcessor<XTextTable> proc, ProgressCounter progress) throws Exception {
+    /**
+     * Обрабатывает все таблицы в заданном отрезке текста.
+     *
+     * @param range отрезок
+     * @param processor обработчик таблицы
+     * @param progress счётчик прогресса
+     */
+    public TextDocument processTablesInsideRange(XTextRange range, ObjectProcessor<XTextTable> processor, ProgressCounter progress) throws Exception {
         XTextTablesSupplier xSup = UnoRuntime
                 .queryInterface(XTextTablesSupplier.class, xDoc);
         XNameAccess textTables = xSup.getTextTables();
@@ -151,12 +233,15 @@ public class TextDocument {
         AtomicInteger i = new AtomicInteger(0);
         textTablesInRange.forEach((k, v) -> {
             progress.next();
-            proc.process(v, xDoc);
+            processor.process(v, xDoc);
         });
 
         return this;
     }
 
+    /**
+     * Обновляет все индексы в документе.
+     */
     public TextDocument updateAllIndexes() {
         XDocumentIndexesSupplier xSup = UnoRuntime
                 .queryInterface(XDocumentIndexesSupplier.class, xDoc);
@@ -176,6 +261,11 @@ public class TextDocument {
         return this;
     }
 
+    /**
+     * Возвращает поток с секциями. Необходимо для обработки таблиц
+     * внутри секций с названиями на <code>tbl:</code>.
+     * @return поток с секциями
+     */
     public Stream<XTextSection> streamSections() throws Exception {
         if (sections.isEmpty())
             scanAllSections();
