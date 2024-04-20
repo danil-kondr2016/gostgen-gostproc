396088c908cac4014e2014f7c8266c019ac347ab
Danila Kondratenko <dan.kondratenko2013@ya.ru>
2024-04-20 15:58:41 +0600

    DefaultProgressInformer: добавление setProgressString и setSilent

--- src/main/java/ru/danilakondr/templater/progress/DefaultProgressInformer.java
+++ src/main/java/ru/danilakondr/templater/progress/DefaultProgressInformer.java
@@ -1,14 +1,26 @@
 package ru.danilakondr.templater.progress;
 
 public class DefaultProgressInformer implements ProgressInformer {
-    private final String progressString;
+    private String progressString;
+    private boolean silent = false;
 
     public DefaultProgressInformer(String progressString) {
         this.progressString = progressString;
     }
 
+    public void setProgressString(String progressString) {
+        this.progressString = progressString;
+    }
+
+    public void setSilent(boolean silent) {
+        this.silent = silent;
+    }
+
     @Override
     public void inform(int current, int total) {
+        if (silent)
+            return;
+
         if (current == -1 && total == -1)
             System.out.println(progressString + "...");
         else if (total == -1)
